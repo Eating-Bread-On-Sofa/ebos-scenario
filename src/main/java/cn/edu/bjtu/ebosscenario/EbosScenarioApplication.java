04ab14f94a9b97a09c5c7499dfa2101d199d615a
Voo-C <voodoo.7@outlook.com>
2020-04-08 15:27:06 +0800

    init

---
+++ src/main/java/cn/edu/bjtu/ebosscenario/EbosScenarioApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.bjtu.ebosscenario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbosScenarioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EbosScenarioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/edu/bjtu/ebosscenario/EbosScenarioApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.bjtu.ebosscenario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbosScenarioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
