458b46eb6e0f96577ab7a94ab1aefde0d71befcb
Cypo <voodoo.7@outlook.com>
2020-04-11 13:23:56 +0800

    API for getting status

--- src/main/java/cn/edu/bjtu/ebosscenario/EbosScenarioApplication.java
+++ src/main/java/cn/edu/bjtu/ebosscenario/EbosScenarioApplication.java
@@ -2,12 +2,24 @@ package cn.edu.bjtu.ebosscenario;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class EbosScenarioApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EbosScenarioApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        SimpleClientHttpRequestFactory simpleClientHttpRequestFactory = new SimpleClientHttpRequestFactory();
+        simpleClientHttpRequestFactory.setConnectTimeout(5000);
+        simpleClientHttpRequestFactory.setReadTimeout(3000);
+        RestTemplate restTemplate = new RestTemplate(simpleClientHttpRequestFactory);
+        return restTemplate;
     }
 
 }
--- src/main/java/cn/edu/bjtu/ebosscenario/controller/ScenarioController.java
+++ src/main/java/cn/edu/bjtu/ebosscenario/controller/ScenarioController.java
@@ -6,6 +6,7 @@ import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
@@ -14,7 +15,10 @@ import java.util.List;
 public class ScenarioController {
     @Autowired
     ScenarioService scenarioService;
+    @Autowired
+    RestTemplate restTemplate;
 
+    @CrossOrigin
     @PostMapping
     public boolean add(@RequestBody JSONObject jsonObject) {
         String name = jsonObject.getString("name");
@@ -25,11 +29,13 @@ public class ScenarioController {
         return scenarioService.addScenario(scenario);
     }
 
-    @DeleteMapping("/{name}")
+    @CrossOrigin
+    @DeleteMapping("/name/{name}")
     public boolean delete(@PathVariable String name){
         return scenarioService.deleteByName(name);
     }
 
+    @CrossOrigin
     @PutMapping
     public void change(@RequestBody JSONObject jsonObject){
         String name = jsonObject.getString("name");
@@ -40,14 +46,37 @@ public class ScenarioController {
         scenarioService.changeScenario(scenario);
     }
 
-    @GetMapping("/{name}")
+    @CrossOrigin
+    @GetMapping("/name/{name}")
     public Scenario findOneScenario(@PathVariable String name){
         return scenarioService.findByName(name);
     }
 
+    @CrossOrigin
     @GetMapping
     public List<Scenario> findAll(){
         return scenarioService.findAll();
     }
 
+    @CrossOrigin
+    @GetMapping("/status/{name}")
+    public JSONArray getStatus(@PathVariable String name){
+        JSONArray readings = new JSONArray();
+        Scenario scenario = scenarioService.findByName(name);
+        JSONArray content = scenario.getContent();
+        for (int i = 0; i < content.size(); i++) {
+            JSONObject gateway = content.getJSONObject(i);
+            String ip = gateway.getString("gatewayIP");
+            JSONArray commands = gateway.getJSONArray("commands");
+            for (int j = 0; j < commands.size(); j++) {
+                JSONObject command = commands.getJSONObject(j);
+                String deviceName = command.getString("deviceName");
+                String commandName = command.getString("commandName");
+                String url = "http://"+ip+":48082/api/v1/device/name/"+deviceName+"/command/"+commandName;
+                JSONObject result = restTemplate.getForObject(url,JSONObject.class);
+                readings.addAll(result.getJSONArray("readings"));
+            }
+        }
+        return readings;
+    }
 }
